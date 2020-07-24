ab50a92b081ae66471ea78b9bad3c81366c9370c
Mike Sardonini <sardonini.michael@aurora.aero>
2020-07-23 21:57:05 -0400

    added docker stuff to git

---
+++ docker/docker_build.sh
@@ -0,0 +1,1 @@
+docker build -t fly_stereo:0.0.1 --network=host .
---
+++ docker/docker_run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ARGS="--env DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  --device /dev/dri \
+  --env XAUTHORITY=$XAUTHORITY \
+  --env DISPLAY=$DISPLAY \
+  -e NVIDIA_DRIVER_CAPABILITIES=all --privileged \
+  -it \
+  --rm \
+  --env DDS_DOMAIN_ID=$DDS_DOMAIN_ID \
+  --network=host \
+  --cap-add=SYS_PTRACE \
+  --ipc=host\
+  --runtime=nvidia \
+  --security-opt seccomp=unconfined \
+  -v /home/msardonini/git/fly_stereo:/root/fly_stereo "
+
+xhost +local:root
+
+docker run $ARGS fly_stereo:0.0.1 
+
+xhost -local:root
